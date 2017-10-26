729c76cd1a8102b6786147211976d1a9172408ce
Madalyn Marabella <mmarabella@utexas.edu>
2017-10-25 21:55:57 -0500

    updated comments

--- WebScraping_CNNMoney.py
+++ WebScraping_CNNMoney.py
@@ -6,15 +6,15 @@ except:
 
 from bs4 import BeautifulSoup
 
-# specify the url
+# specify urls
 DJ_page = 'http://money.cnn.com/data/markets/dow/'
 CC_page = 'http://markets.money.cnn.com/Marketsdata/Sectors/ConsumerCyclical'
 
-# query website and return html to variable 'page'
+# query websites and return html to variables
 DJ_page = urlopen(DJ_page)
 CC_page = urlopen(CC_page)
 
-# parse the html using beautiful soap and store in variable 'soup'
+# parse the html using beautiful soup and store in variables 'DJ_soup' and 'CC_soup'
 DJ_soup = BeautifulSoup(DJ_page, 'html.parser')
 CC_soup = BeautifulSoup(CC_page, 'html.parser')
 
@@ -31,7 +31,7 @@ print(CC_change)
 import csv
 from datetime import datetime
 
-# open a csv file with append, so old data will not be erased
+# open a csv file and append data
 with open('WebScraping_CNN.csv', 'a') as csv_file:
 	writer = csv.writer(csv_file)
 	writer.writerow([datetime.now(), DJ_change, CC_change,])
